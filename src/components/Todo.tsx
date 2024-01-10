f248c0a110fd5361d092a84412cc2303713b6549
JoonPark <sargadil@gmail.com>
2024-01-10 17:43:34 +0900

    Todo - mutation concept

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,30 +1,41 @@
-import { useTodosIds } from "../services/queries";
+import { useTodos, useTodosIds } from "../services/queries";
 
 export default function Todo() {
   const todosIdsQuery = useTodosIds();
-
-  // if (todosIdsQuery.isLoading) return <div>Loading...</div>;
-
-  // if (todosIdsQuery.isError) return <div>there is an Error!</div>;
+  const todosQueries = useTodos(todosIdsQuery.data);
 
   return (
     <div>
-      <p>Query function status : {todosIdsQuery.fetchStatus}</p>
-      <p>Query data status : {todosIdsQuery.status}</p>
-      
-      {todosIdsQuery.data?.map((id) => (
-        <p key={id}>{id}</p>
-      ))}
+      <ul>
+        {todosQueries.map(({ data }) => (
+          <li key={data?.id}>
+            <div> Id : {data?.id}</div>
+            <span>
+              <strong>Title : </strong> {data?.title},{" "}
+              <strong>Description : </strong> {data?.description}
+            </span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
 
+/* const todosIdsQuery = useTodosIds();
+   const isFetching = useIsFetching();
+   if (todosIdsQuery.isLoading) return <div>Loading...</div>;
+   if (todosIdsQuery.isError) return <div>there is an Error!</div>;
+   <p>Query function status : {todosIdsQuery.fetchStatus}</p>
+   <p>Query data status : {todosIdsQuery.status}</p>
+   <p>Global isFetching : {isFetching}</p>
+   {todosIdsQuery.data?.map((id) => (
+    <p key={id}>id : {id}</p>
+   ))} */
+
+
 //   const { data, isPending, isError } = useTodosIds();
-
 //   if (isPending) return <div>Loading...</div>;
-
 //   if (isError) return <div>there is an Error!</div>;
-
 //   return (
 //     <div>
 //       {data?.map((id) => (
--- src/services/queries.ts
+++ src/services/queries.ts
@@ -1,9 +1,20 @@
-import { useQuery } from "@tanstack/react-query";
-import { getTodosIds } from "./api";
+import { useQueries, useQuery } from "@tanstack/react-query";
+import { getTodosIds, getTodo } from "./api";
 
-export function useTodosIds(){
+export function useTodosIds() {
   return useQuery({
-    queryKey:["todos"],
+    queryKey: ["todos"],
     queryFn: getTodosIds,
+  });
+}
+
+export function useTodos(ids: (number | undefined)[] | undefined) {
+  return useQueries({
+    queries: (ids ?? [])?.map((id) => {
+      return {
+        queryKey: ["todos", id],
+        queryFn: () => getTodo(id!),
+      };
+    }),
   });
 }
